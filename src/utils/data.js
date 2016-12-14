76fb6596c52c0a87e3628511db28817d99b9c6aa
Tom Wilson <tom@jackhq.com>
2016-12-14 10:06:11 -0500

    added data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,18 +1,17 @@
 const fetch = require('isomorphic-fetch')
 
 module.exports = function () {
+  const setAuth = () => 'Bearer ' + window.localStorage.getItem('id_token')
   const get = (model, id) =>
     fetch(`http://localhost:4000/${model}/${id}`, {
       headers: {
-        Authorization: 'Bearer ' + window.localStorage.getItem('id_token')
+        Authorization: setAuth()
       }
     })
+      .then(res => res.json())
 
 
   return {
-    get,
-    list,
-    post,
-    put
+    get
   }
 }
